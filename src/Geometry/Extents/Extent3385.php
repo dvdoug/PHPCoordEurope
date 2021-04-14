ba71efe9bc899ca2e0fb94499954af70b29fdaf1
Doug Wright <github@dougweb.org>
2021-04-14 20:18:40 +0100

    Add polygons

---
+++ src/Geometry/Extents/Extent3385.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\Geometry\Extents;
+
+/**
+ * Europe-FSU/Finland - east of 31.5°E.
+ * @internal
+ */
+class Extent3385
+{
+    public function __invoke(): array
+    {
+        return
+        [
+            [
+                [
+                    [31.5, 62.83438575901], [31.501388549805, 62.835548400879], [31.521663665772, 62.853332519531], [31.58196258545, 62.907897949219], [31.519582748413, 62.982635498047], [31.5, 62.995045939351], [31.5, 62.83438575901],
+                ],
+            ],
+        ];
+    }
+}
