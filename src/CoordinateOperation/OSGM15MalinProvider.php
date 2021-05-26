a88a4c8ec0005bc8a123d2e4e2a23e43f4d03661
Doug Wright <github@dougweb.org>
2021-05-26 22:44:08 +0100

    Add Ireland OSGM15 grids

---
+++ src/CoordinateOperation/OSGM15MalinProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\CoordinateOperation;
+
+class OSGM15MalinProvider implements GridProvider
+{
+    private OSGM15IrelandGrid $cache;
+
+    public function provideGrid(): OSGM15IrelandGrid
+    {
+        return $this->cache ??= new OSGM15IrelandGrid(__DIR__ . '/../../resources/OSGM15_Malin.gri');
+    }
+}
