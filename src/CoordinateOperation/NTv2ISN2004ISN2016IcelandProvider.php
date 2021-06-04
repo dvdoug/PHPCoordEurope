7f4a43f232c7ef9851a882de2a4f9a31d1ae12d7
Doug Wright <github@dougweb.org>
2021-06-04 23:02:55 +0100

    Add grids for Iceland

---
+++ src/CoordinateOperation/NTv2ISN2004ISN2016IcelandProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\CoordinateOperation;
+
+class NTv2ISN2004ISN2016IcelandProvider implements GridProvider
+{
+    private NTv2Grid $cache;
+
+    public function provideGrid(): NTv2Grid
+    {
+        return $this->cache ??= new NTv2Grid(__DIR__ . '/../../resources/ISN2004_ISN2016.gsb');
+    }
+}
---
+++ src/CoordinateOperation/NTv2ISN93ISN2016IcelandProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\CoordinateOperation;
+
+class NTv2ISN93ISN2016IcelandProvider implements GridProvider
+{
+    private NTv2Grid $cache;
+
+    public function provideGrid(): NTv2Grid
+    {
+        return $this->cache ??= new NTv2Grid(__DIR__ . '/../../resources/ISN93_ISN2016.gsb');
+    }
+}
