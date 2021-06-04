dc10e4cc7717e5e1ee3407f6903765a56c7764fd
Doug Wright <github@dougweb.org>
2021-06-04 21:47:09 +0100

    Add Portugal grids

---
+++ src/CoordinateOperation/NTv273ETRS89PortugalProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\CoordinateOperation;
+
+class NTv273ETRS89PortugalProvider implements GridProvider
+{
+    private NTv2Grid $cache;
+
+    public function provideGrid(): NTv2Grid
+    {
+        return $this->cache ??= new NTv2Grid(__DIR__ . '/../../resources/D73_ETRS89_geo.gsb');
+    }
+}
