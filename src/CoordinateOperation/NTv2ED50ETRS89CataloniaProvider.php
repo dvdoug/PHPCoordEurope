f625253de6454a5d71e07d7d7e2c165465aae803
Doug Wright <github@dougweb.org>
2021-06-04 20:55:36 +0100

    Add Catalonia grid

---
+++ src/CoordinateOperation/NTv2ED50ETRS89CataloniaProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\CoordinateOperation;
+
+class NTv2ED50ETRS89CataloniaProvider implements GridProvider
+{
+    private NTv2Grid $cache;
+
+    public function provideGrid(): NTv2Grid
+    {
+        return $this->cache ??= new NTv2Grid(__DIR__ . '/../../resources/100800401.gsb');
+    }
+}
