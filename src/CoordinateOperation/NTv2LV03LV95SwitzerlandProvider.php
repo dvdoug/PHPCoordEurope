92a714a945f2abba50d05f8af9fa0fb31d8f68c7
Doug Wright <github@dougweb.org>
2021-06-04 17:36:26 +0100

    Add Swiss grids

---
+++ src/CoordinateOperation/NTv2LV03LV95SwitzerlandProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\CoordinateOperation;
+
+class NTv2LV03LV95SwitzerlandProvider implements GridProvider
+{
+    private NTv2Grid $cache;
+
+    public function provideGrid(): NTv2Grid
+    {
+        return $this->cache ??= new NTv2Grid(__DIR__ . '/../../resources/CHENyx06a.gsb');
+    }
+}
