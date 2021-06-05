1e0f823eb6d428770c261b6773e9515f2c4e2894
Doug Wright <github@dougweb.org>
2021-06-05 17:43:02 +0100

    Add grid for Saarland

---
+++ src/CoordinateOperation/NTv2DHDNETRS89SaarlandProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\CoordinateOperation;
+
+class NTv2DHDNETRS89SaarlandProvider implements GridProvider
+{
+    private NTv2Grid $cache;
+
+    public function provideGrid(): NTv2Grid
+    {
+        return $this->cache ??= new NTv2Grid(__DIR__ . '/../../resources/SeTa2016.gsb');
+    }
+}
