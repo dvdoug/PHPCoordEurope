996cee139b947114bc4530122c7b7f58f0dc0129
Doug Wright <github@dougweb.org>
2021-10-09 20:08:17 +0100

    Add Spanish height grids

---
+++ src/CoordinateOperation/IGNESHeightETRS89REDNAPCanaryIslandsProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\CoordinateOperation;
+
+class IGNESHeightETRS89REDNAPCanaryIslandsProvider implements GridProvider
+{
+    private IGNESHeightGrid $cache;
+
+    public function provideGrid(): IGNESHeightGrid
+    {
+        return $this->cache ??= new IGNESHeightGrid(__DIR__ . '/../../resources/EGM08_REDNAP_Canarias.txt');
+    }
+}
