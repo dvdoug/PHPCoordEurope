0d30b05fedf9e312dfb90974c084d8696ec8b881
Doug Wright <github@dougweb.org>
2022-09-03 20:57:32 +0100

    Add Iceland vertical grid

---
+++ src/CoordinateOperation/GTXISN2016ISH2004IcelandProvider.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\CoordinateOperation;
+
+class GTXISN2016ISH2004IcelandProvider implements GridProvider
+{
+    public function provideGrid(): GTXGrid
+    {
+        return new GTXGrid(__DIR__ . '/../../resources/Icegeoid_ISN2016.gtx');
+    }
+}
