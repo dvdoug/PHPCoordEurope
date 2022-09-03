7123e552d8d4f1a0736fdb4075aa9bc22cf81b41
Doug Wright <github@dougweb.org>
2022-09-03 20:55:28 +0100

    Update extents

---
+++ src/Geometry/Extents/Extent4663.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\Geometry\Extents;
+
+/**
+ * Europe-FSU/UK - Leeds to Hull.
+ * @internal
+ */
+class Extent4663
+{
+    public function __invoke(): array
+    {
+        return
+        [
+            [
+                [
+                    [-1.65, 53.9], [-1.695833333, 53.6], [-1.2402777776666, 53.6], [-0.78472222233328, 53.6], [-0.32916666699992, 53.6], [-0.27083333299993, 53.9], [-0.73055555533329, 53.9], [-1.1902777776666, 53.9], [-1.65, 53.9],
+                ],
+            ],
+        ];
+    }
+}
